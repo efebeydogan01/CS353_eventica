d0306699bfa99d87d3ec5a5a2ff5d9d274d550e8
efebeydogan01 <efe.beydogan@ug.bilkent.edu.tr>
2022-12-19 21:03:04 +0300

    setup database connection, tables are still missing

---
+++ eventica/events/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Path converters: int, str, path, slug, UUID
+    path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home"),
+]
